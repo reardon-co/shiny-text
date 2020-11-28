030175ba63e39ffa3974ed438e2a95984ab62bbd
Cole Reardon <reardon.co@northeastern.edu>
2020-11-28 14:52:47 -0500

    removed uneeded files + changed animation style

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@ const text = document.querySelector(".text-gradient");
 
 
 // Checking the position of the mouse inside the text div
-text.addEventListener("mousemove", (event) => {
+document.addEventListener("mousemove", (event) => {
 
   let mousePercent = (event.pageX - text.offsetLeft) / text.offsetWidth * 100;
 
   // console.log(mousePercent);
-  let leftLeftGrad = mousePercent - 80;
+  let leftLeftGrad = mousePercent - 100;
   let leftGrad = mousePercent - 5;
   let rightGrad = mousePercent + 5;
-  let rightRightGrad = mousePercent + 80;
+  let rightRightGrad = mousePercent + 100;
 
   var backgroundStyle = 
     'linear-gradient(110deg, #b721ff ' + 
